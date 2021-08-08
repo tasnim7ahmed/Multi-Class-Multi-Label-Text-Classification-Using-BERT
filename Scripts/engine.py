361ed7154d7c37c8fd6fb91d224a421404359da1
Tasnim Ahmed <tasnimahmed@iut-dhaka.edu>
2021-08-08 21:24:23 +0600

    Changed Torch Metrics for Multi Label

--- Scripts/engine.py
+++ Scripts/engine.py
@@ -4,6 +4,7 @@ from tqdm import tqdm
 import time
 import numpy as np
 from sklearn.metrics import f1_score
+import torchmetrics
 
 import utils
 from common import get_parser
@@ -58,8 +59,7 @@ def train_fn(data_loader, model, optimizer, device, scheduler):
         progrss_bar.set_postfix(loss = losses.avg)
         final_target.extend(target.cpu().detach().numpy().tolist())
         final_output.extend(output.cpu().detach().numpy().tolist())
-    f1 = f1_score(final_target, final_output, average='weighted')
-    f1 = np.round(f1.item(), 4)
+    f1 = torchmetrics.F1(final_target, final_output, average='weighted')
     return f1, np.mean(train_losses)
 
 def eval_fn(data_loader, model, device):
@@ -92,8 +92,7 @@ def eval_fn(data_loader, model, device):
             val_losses.append(loss.item())
             final_target.extend(target.cpu().detach().numpy().tolist())
             final_output.extend(output.cpu().detach().numpy().tolist())
-    f1 = f1_score(final_target, final_output, average='weighted')
-    f1 = np.round(f1.item(), 4)
+    f1 = torchmetrics.F1(final_target, final_output, average='weighted')
     return f1, np.mean(val_losses)
 
 def test_eval_fn(data_loader, model, device):
